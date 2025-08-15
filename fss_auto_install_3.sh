2bb6032e6997195d8ac355578bb2f85b82150b43
maionesos <twinkcy@yandex.ru>
2025-08-15 14:19:15 +0300

    fix inputval

--- fss_auto_install_3.sh
+++ fss_auto_install_3.sh
@@ -1,15 +1,8 @@
  #!/bin/bash
-<<<<<<< HEAD
- 
-if [ $(whoami) = root ] ; then 
-	echo 
-else 
-=======
 
 if [ $(whoami) = root ] ; then
 	echo
 else
->>>>>>> 46ff7eb (initial commit)
 	echo -en "$color1b Нужно открыть скрипт рутом! Установка прервана. $color1e"
 	exit 1
 fi
@@ -24,11 +17,7 @@ color1e="\033[0m"
 color2b="\033[37;1;42m"
 color2e="\033[0m"
 
-<<<<<<< HEAD
-echo -e "Установка АРМ ФСС"
-=======
 echo -e "Установка АРМ ФСС на Альт Линукс!"
->>>>>>> 46ff7eb (initial commit)
 echo -en "$color2b Введите 'yes' для продолжения: $color2e"
 read inputval
 if test "$inputval" != "yes"
@@ -42,17 +31,6 @@ user1=$(who | grep '(:0)' | cut -d " " -f1)
 
 mkdir /opt/certs
 chmod 777 /opt/certs
-<<<<<<< HEAD
-cd /tmp/ 
-env -i wget -nv --no-cache http://lk.fss.ru/sfr_certs_2023/cert.cer
-
-
-if rpm -qa | grep -q postgresql9.4 ; then 
-	echo "Уже имеется postgresql старой версии нужно удалить её и сделать бекап"
-	echo -n " 'yes' для отмены установки или 'del' для удаления postgresql9.4 (бекапы не делаются):"
-read inputval1
-if test "$inputval1" == "yes" 
-=======
 env -i wget -nv --no-cache -P /tmp/ http://lk.fss.ru/sfr_certs_2025/sfr_prod_cert_2025.cer
 mv /tmp/sfr_prod_cert_2025.cer /opt/certs/cert.cer
 
@@ -63,7 +41,6 @@ if rpm -qa | grep -q postgresql9.4; then
 	echo -n " 'yes' для отмены установки или 'del' для удаления postgresql9.4 (бекапы не делаются!):"
 read inputval1
 if test "$inputval1" == "yes"
->>>>>>> 46ff7eb (initial commit)
 then
 	echo -en "$color1b Установка отменена :с $color1e"
 	exit 1
@@ -73,74 +50,6 @@ then
 fi
 fi
 
-<<<<<<< HEAD
-##################################################
-
-echo -en "$color1b Устанавливаем локальную бд: yes/no?? $color2e"
-read inputval2
-if test "$inputval2" == "no" 
-then
-	echo
-
-elif test "$inputval2" == "yes"
-	then
-
-env -i apt-get update; env -i apt-get -y dist-upgrade
-env -i apt-get install -y postgresql10-server
-
-/etc/init.d/postgresql initdb
-
-echo listen_addresses = "'*'" >> /var/lib/pgsql/data/postgresql.conf
-sed -i 's/5432/5433/g' /lib/systemd/system/postgresql.service
-systemctl daemon-reload
-
-systemctl enable postgresql; systemctl restart postgresql
-
-psql -p 5433 -U postgres -c "CREATE USER fss WITH SUPERUSER LOGIN;"
-psql -p 5433 -U postgres -c "CREATE DATABASE fss WITH ENCODING='UTF-8';"
-
-#####Пустая бд пострегреса.
-cd /tmp/
-echo -en "$color2b Бд создается для ЭЛН(eln) или ЭРС(ers)?$color2e"
-read inputval3
-
-####Добавить бекап локальной бд!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-if test "$inputval3" == "eln"
-then
-		cd /tmp/; env -i wget -nv --no-cache http://10.11.128.115/.pcstuff/test/backup_enl_null.sql	
-		psql -p 5433 -U postgres -d "fss" -f /tmp/backup_enl_null.sql
-		psql -p 5433 -U postgres -c "ALTER DATABASE "fss" OWNER TO "fss";" 
-
-elif test "$inputval3" == "ers"
-	then
-		cd /tmp/; env -i wget -nv --no-cache http://10.11.128.115/.pcstuff/test/backup_ers_empty.sql	
-		psql -p 5433 -U postgres -d "fss" -f /tmp/backup_ers_empty.sql
-		psql -p 5433 -U postgres -c "ALTER DATABASE "fss" OWNER TO "fss";"
-
-echo -en "$color2b Если нужна была только БД, то можно прервать установку, прерываем? $color2e"
-echo -n " 'cancel' для отмены установки или 'go' для продолжения:"
-read inputvalBD
-if test "$inputvalBD" == "cancel" 
-then
-	exit 1
-elif test "$inputvalBD" == "yes"
-	then
-	echo
-fi
-fi
-fi
-
-##################################################
-
-#чек версии wine
-check5=$(wine --version | head -n1  | awk '{print $1;}' | cut -d "-" -f2)
-check6="8.0.6"
-check_bottle=/home/$user1/.wine.fss
-
-if [[ ${check5} == ${check6} ]]; then
-  if [ -d $check_bottle ]; then
-=======
 check_bd_table () {
     if psql -p 5433 -U postgres -lqt | grep -qw fss; then
       echo -en "$color2b Таблица ФСС уже существует в БД, сносить(del) или оставить её(go)?$color2e"
@@ -156,13 +65,15 @@ psql -p 5433 -U postgres -c "CREATE USER fss WITH SUPERUSER LOGIN;"
 psql -p 5433 -U postgres -c "CREATE DATABASE fss WITH ENCODING='UTF-8';"
 }
 
+test_file=/tmp/fss_log_bd
+
 install_bd_postgres() {
     echo -en "$color1b Устанавливаем локальную бд: yes/no?? $color2e"
     read inputval2
     if test "$inputval2" == "no"; then
       echo
     elif test "$inputval2" == "yes"; then
-
+    touch "$test_file"
     env -i apt-get update; env -i apt-get -y dist-upgrade
     env -i apt-get install -y postgresql10-server
 
@@ -179,7 +90,7 @@ install_bd_postgres() {
 
       if test "$inputvalBD" == "cancel"; then
         exit 1
-      elif test "$inputvalBD" == "yes"
+      elif test "$inputvalBD" == "go"
         then
           echo
       fi
@@ -215,13 +126,17 @@ install_fss_wine_8() {
     echo -en "$color2b Выберите номер программы: ФСС ЭРС - 1; ФСС ЭЛН - 2: $color2e"
     read inputval5
     if [ "$inputval5" == "1" ]; then
-        install_bd_for_ers
         su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine.fss.ers.tar.gz"
         su - ${user1} -c "tar -xf wine.fss.ers.tar.gz"
+        if [ -f "$test_file" ]; then
+            install_bd_for_ers
+        fi
     elif [ "$inputval5" == "2" ]; then
-        install_bd_for_eln
         su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine.fss.eln.tar.gz"
         su - ${user1} -c "tar -xf wine.fss.eln.tar.gz"
+        if [ -f "$test_file" ]; then
+            install_bd_for_eln
+        fi
     fi
 }
 
@@ -229,13 +144,17 @@ install_fss_wine_10() {
     echo -en "$color2b Выберите номер программы: ФСС ЭРС - 1; ФСС ЭЛН - 2: $color2e"
     read inputval5
     if [ "$inputval5" == "1" ]; then
-        install_bd_for_ers
         su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine-10.8.1_fss_ers_x32.tar.gz"
         su - ${user1} -c "tar -xf wine-10.8.1_fss_ers_x32.tar.gz"
+        if [ -f "$test_file" ]; then
+            install_bd_for_ers
+        fi
     elif [ "$inputval5" == "2" ]; then
-        install_bd_for_eln
         su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine-10.8.1_fss_eln.tar.gz"
         su - ${user1} -c "tar -xf wine-10.8.1_fss_eln.tar.gz"
+        if [ -f "$test_file" ]; then
+            install_bd_for_eln
+        fi
     fi
 }
 
@@ -247,48 +166,10 @@ check_bottle=/home/$user1/.wine.fss
 
 if [[ ${check_wine} == ${check_wine1} ]]; then
   if [[ -d $check_bottle ]]; then
->>>>>>> 46ff7eb (initial commit)
     i=1
     while [ -d $check_bottle$i ]; do
       i=$((i+1))
     done
-<<<<<<< HEAD
-	mv /home/${user1}/.wine.fss /home/${user1}/.wine.fss.bak$1
-  fi
-else
-	result_message_wine="wine $check5 - Версия wine не соответствует требуемой, установка прервана :("
-	exit 0
-fi
-
-##########################################################
-
-echo -en "$color2b Выберите номер программы: ФСС ЭРС - 1; ФСС ЭЛН - 2:$color2e"
-read inputval5
-
-if [ "$inputval5" == "1" ]; then
-    su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine.fss.ers.tar.gz"
-    su - ${user1} -c "tar -xf wine.fss.ers.tar.gz"
-elif [ "$inputval5" == "2" ]; then
-    su - ${user1} -c "cd /home/'$user1'/ && env -i wget --progress=bar:force --no-cache http://10.11.128.115/.pcstuff/test/fss/wine.fss.eln.tar.gz"
-    su - ${user1} -c "tar -xf wine.fss.eln.tar.gz"
-fi
-
-cat << '_EOF_' >  /usr/bin/run_fss.sh
-#!/bin/bash
-/opt/cprocsp/bin/amd64/certmgr -delete -all -store uMy
-/opt/cprocsp/bin/amd64/csptestf -absorb -certs
-/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -file "/opt/certs/cert.cer"
-env WINEPREFIX=$HOME/.wine.fss wine reg delete 'HKEY_CURRENT_USER\Software\Microsoft\SystemCertificates\My\Certificates\' /f
-if [ -f /usr/lib/wine/i386-unix/cpcsp_proxy_setup.exe.so ];then
- 	WINEPREFIX=$HOME/.wine.fss wine /usr/lib/wine/i386-unix/cpcsp_proxy_setup.exe.so one1 two1 three1	
-else
-	WINEPREFIX=$HOME/.wine.fss wine /usr/lib/wine/cpcsp_proxy_setup.exe.so one1 two1	
-fi
-cd $HOME/.wine.fss/drive_c/Fss*
-env WINEPREFIX=$HOME/.wine.fss wine fss_mo.exe
-_EOF_
-
-=======
     mv /home/${user1}/.wine.fss /home/${user1}/.wine.fss.bak$1
   fi
   install_fss_wine_8
@@ -307,7 +188,6 @@ fi
 
 env -i wget -nv --no-cache -P /tmp/ http://10.11.128.115/.pcstuff/test/fss/run_fss_wine_10.sh
 mv /tmp/run_fss_wine_10.sh /usr/bin/run_fss.sh
->>>>>>> 46ff7eb (initial commit)
 chmod 755 /usr/bin/run_fss.sh
 
 env -i wget -nv --no-cache http://10.11.128.115/.pcstuff/gui/icons/fsslogo.ico
@@ -327,20 +207,12 @@ _EOF_
 chown $user1:$user1 /home/$user1/Рабочий\ стол/АРМ\ ЛПУ.desktop
 chmod +x /home/$user1/Рабочий\ стол/АРМ\ ЛПУ.desktop
 
-<<<<<<< HEAD
-#########################################################
-=======
 # Cоздаем ярлычки
->>>>>>> 46ff7eb (initial commit)
 
 rm -f "/home/'$user1'/.local/share/applications/wine/Programs/СФР АРМ ЛПУ"
 rm -f "/home/'$user1'/.local/share/applications/wine/Programs/СФР АРМ ЛПУ(ЭРС)"
 rm -f /home/$user1/.local/share/applications/wine/Programs/wine-Programs-СФР*
 rm -f /home/$user1/Рабочий\ стол/СФР\ АРМ\ ЛПУ.desktop
-<<<<<<< HEAD
-#rm -f /home/$user1/Рабочий\ стол/СФР\ АРМ\ ЛПУ(ЭРС\).desktop
-=======
->>>>>>> 46ff7eb (initial commit)
 rm -f /home/$user1/wine.fss.*.tar.gz
 
 echo -en "ФСС Установлено!"
